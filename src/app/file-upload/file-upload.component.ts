e7d3351d8f97ea54ebf8453c13550c035b120e1e
Avinash <avinashg12291992@gmail.com>
2018-05-11 00:04:41 +0530

    hiding thumbnail by default

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -68,6 +68,7 @@ export class FileUploadComponent implements OnInit {
   readUrl(event: any, file: File) {
     const reader = new FileReader();
     reader.onload = function(e: any) {
+      document.getElementById('thumbnail').removeAttribute('hidden');
       document.getElementById('thumbnail').setAttribute('src', e.target.result);
       // console.log(e.target.result);
     };
