f0c9a52a80ba837d159e35bd96645887981416eb
Avinash <avinashg12291992@gmail.com>
2018-05-10 23:57:07 +0530

    removing config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,5 @@ export const environment = {
   production: false,
   firebase: {
     // Add your firebase config here.
-    apiKey: 'AIzaSyAet81GzCmcml4QZpT6qaIY_OQhjMFSBCk',
-    authDomain: 'angular-fileupload.firebaseapp.com',
-    databaseURL: 'https://angular-fileupload.firebaseio.com',
-    projectId: 'angular-fileupload',
-    storageBucket: 'angular-fileupload.appspot.com',
-    messagingSenderId: '769626972540'
   }
 };
